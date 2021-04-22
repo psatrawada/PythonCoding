916a202f10534c19fed4d87c7107775f6e15d2df
psatrawada <psatrawada@gmail.com>
2021-04-21 21:44:57 -0700

    re

---
+++ StringManipulations/ReverString.py
@@ -0,0 +1,19 @@
+#Reverse a string
+#input:  "HELLO World"
+#output:
+def rev(s):
+    newstring = "";
+    for i in s:
+        newstring = i + newstring;
+    return (newstring)
+
+def revRecursive(s):
+    if len(s)== 0 :
+        return(s)
+    else:
+        return (revRecursive(s[1:]) + s[0])
+
+
+S = "Hello World"
+print(S, "is reversed as ", rev(S) );
+print(S, "is reversed using recursive ", revRecursive(S) );
